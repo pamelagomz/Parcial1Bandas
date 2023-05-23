4c38fd248ccf2c66503471253837b28df3688fdd
pamelagomz <00170221@uca.edu.sv>
2023-05-23 09:46:32 -0600

    :construction:Adaptador Recycler View

--- app/src/main/java/com/pgomez/parcial1bandas/ui/band/home/recyclerview/BandRecyclerViewAdapter.kt
+++ app/src/main/java/com/pgomez/parcial1bandas/ui/band/home/recyclerview/BandRecyclerViewAdapter.kt
@@ -1,4 +1,44 @@
 package com.pgomez.parcial1bandas.ui.band.home.recyclerview
 
-class BandRecyclerViewAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pgomez.parcial1bandas.data.models.BandModel
+import com.pgomez.parcial1bandas.databinding.BandItemBinding
+
+class BandRecyclerViewAdapter(private val clickListener: (BandModel) -> Unit):
+RecyclerView.Adapter<BandRecyclerViewHolder>()
+{
+    private val bands = ArrayList<BandModel>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BandRecyclerViewHolder {
+        val binding = BandItemBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        return BandRecyclerViewHolder(binding)
+
+    }
+
+    override fun getItemCount(): Int {
+        return bands.size
+    }
+
+    override fun onBindViewHolder(holder: BandRecyclerViewHolder, position: Int) {
+        val band = bands[position]
+        holder.bind(band, clickListener)
+
+    }
+
+    fun setData(bandList: List<BandModel>) {
+        bands.clear()
+        bands.addAll(bandList)
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/pgomez/parcial1bandas/ui/band/home/recyclerview/BandRecyclerViewHolder.kt
+++ app/src/main/java/com/pgomez/parcial1bandas/ui/band/home/recyclerview/BandRecyclerViewHolder.kt
@@ -1,4 +1,17 @@
 package com.pgomez.parcial1bandas.ui.band.home.recyclerview
 
-class BandRecyclerViewHolder {
+import androidx.recyclerview.widget.RecyclerView
+import com.pgomez.parcial1bandas.data.models.BandModel
+import com.pgomez.parcial1bandas.databinding.BandItemBinding
+
+class BandRecyclerViewHolder(private val binding: BandItemBinding): RecyclerView.ViewHolder(binding.root) {
+    fun bind(band: BandModel, clickListener: (BandModel) -> Unit) {
+        binding.textViewName.text = band.name
+        binding.textViewGenre.text = band.genre
+
+
+        binding.bandItemCard.setOnClickListener{
+            clickListener(band)
+        }
+    }
 }
