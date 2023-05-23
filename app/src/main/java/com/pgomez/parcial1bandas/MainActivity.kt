36b949cf68fcb57e5ac8ec964a312ea097305db9
pamelagomz <00170221@uca.edu.sv>
2023-05-23 09:08:22 -0600

    :memo:Commit inicial

---
+++ app/src/main/java/com/pgomez/parcial1bandas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pgomez.parcial1bandas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
